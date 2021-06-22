c886d4b9d9f174dc7a8677ee38d26fed29ef214c
xboxonemaddali9 <72016797+xboxonemaddali9@users.noreply.github.com>
2021-06-22 12:50:58 +1000

    Initial Commit

--- main.py
+++ main.py
@@ -6,21 +6,36 @@ pygame.init()
 size = 700, 700
 width, height = size
 
-window = pygame.display.set_mode(size)
+image_height, image_width = 400, 400
+
+screen = pygame.display.set_mode(size)
 
 running = True
 background = (50, 50, 50)
 
 while True:
     for event in pygame.event.get():
-        # initialising window
+        # initialising screen
         pygame.display.set_caption("Naga's Pygame")
-        window.fill(background)
+        screen.fill(background)
         pygame.display.update()
-        # quitting the window
+        # quitting the screen
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
+        # loading player UI
+        ball = pygame.image.load("download.bmp")
+        ball = pygame.transform.smoothscale(ball, (image_height, image_width))
+        screen.blit(source=ball, area=(0, 0))
+        rect = ball.get_rect()
+        speed = [2, 2]
+
+        rect = rect.move(speed)
+        if rect.left < 0 or rect.right > width:
+            speed[0] = -speed[0]
+        if rect.top < 0 or rect.bottom > height:
+            speed[1] = -speed[1]
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
