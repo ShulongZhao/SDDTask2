1002c9f3691b6271a365b2b65775b1d1ef404084
xboxonemaddali9 <72016797+xboxonemaddali9@users.noreply.github.com>
2021-06-25 23:50:38 +1000

    changed name of files

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import pygame
 from pygame.locals import *
+import time
 
-pygame.init()  # initialise python
-
+pygame.init()
 
 size = [700, 700]
 background = (50, 50, 50)
@@ -11,9 +11,11 @@ screen = pygame.display.set_mode(size)
 
 plyrCoordinates = [200, 200]
 image_height, image_width = 50, 50
-movementIncrement = 2
 
-while True:
+speedMultiplier = 2
+
+gameState = True
+while gameState:
     for event in pygame.event.get():
         # initialising screen
         pygame.display.set_caption("(insert game title here)")
@@ -21,25 +23,26 @@ while True:
 
         # quitting the screen
         if event.type == pygame.QUIT:
-            exit()
+            gameState = False
 
         # loading player character
         plyr = pygame.image.load("Images/playerCharacter.bmp")
         plyr = pygame.transform.scale(plyr, (image_height, image_width))
+        # redraws ball image onto screen
         screen.blit(source=plyr, dest=plyrCoordinates)
-        rect = plyr.get_rect()  # forms a rect around image for collision detection
+        # forms a rect around image for collision detection
+        rect = plyr.get_rect()
 
         # moving player character
-        # returns array of booleans, showing which keys are pressed
-        while event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                plyrCoordinates[0] -= movementIncrement
-            elif event.key == pygame.K_RIGHT:
-                plyrCoordinates[0] += movementIncrement
-            elif event.key == pygame.K_UP:
-                plyrCoordinates[1] -= movementIncrement
-            elif event.key == pygame.K_DOWN:
-                plyrCoordinates[1] += movementIncrement
-            break
+        keys = pygame.key.get_pressed()
+
+        plyrCoordinates[0] += (keys[pygame.K_RIGHT] -
+                               keys[pygame.K_LEFT]) * speedMultiplier
+        plyrCoordinates[1] += (keys[pygame.K_DOWN] -
+                               keys[pygame.K_UP]) * speedMultiplier
 
         pygame.display.update()
+
+
+pygame.quit()
+exit()
