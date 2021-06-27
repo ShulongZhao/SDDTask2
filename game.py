0cd7ef0f45fa63fa297f6f54595ea9983329e16b
xboxonemaddali9 <72016797+xboxonemaddali9@users.noreply.github.com>
2021-06-27 19:06:00 +1000

    Hiding mouse successful

--- game.py
+++ game.py
@@ -15,13 +15,10 @@ def Game(_frameRate, _window, _windowBG, _plyr):
         clock = pygame.time.Clock()
         clock.tick(_frameRate)
 
-        # # non-visble mouse during in game
-        # pygame.mouse.set_visible(mouseVisibility)
-        # #limits all user input to pygame environment
-        # pygame.event.set_grab(limit_external_input)
-        # if (mouseVisibility == False) and (limit_external_input == True):
-        #     #sets cursor to bottom-left of screen
-        #     pygame.mouse.set_pos((0, 720))
+        # non-visble mouse during in game
+        pygame.mouse.set_visible(mouseVisibility)
+        #limits all user input to pygame environment
+        pygame.event.set_grab(limit_external_input)
 
         # window fill before drawing player
         # so player is above window layer
@@ -32,17 +29,21 @@ def Game(_frameRate, _window, _windowBG, _plyr):
             if event.type == pygame.QUIT:
                 # exits loop
                 gameState = False
-            # if event.type == pygame.KEYDOWN:
-            #     if event.key == pygame.K_ESCAPE:
-            #         # mouse visibility is true
-            #         mouseVisibility = True
-            #         limit_external_input = False
-            # if event.type == pygame.MOUSEBUTTONDOWN:
-            #     if (mouseVisibility == True) and (limit_external_input == False):
-            #         mouseVisibility = False
-            #         limit_external_input = True
+            
+            # if player hits escape, mouse is unhidden;
+            # if player hits mouse down when mouse is unhidden,
+            # mouse is hidden again
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    # mouse visibility is true
+                    mouseVisibility = True
+                    limit_external_input = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if (mouseVisibility == True) and (limit_external_input == False):
+                    mouseVisibility = False
+                    limit_external_input = True
 
-        # getting state of all keys
+        # getting state of all  keys
         keys = pygame.key.get_pressed()
         # keys[pygame.(any key)] is always either 0 (if not being pressed) or 1 (if being pressed); boolean value
         # therefore, keys[pygame.K_RIGHT] = 0, keys[pygame.K_LEFT] = 1 --> player x-coordinate = (0 - 1) * speed --> goes left
