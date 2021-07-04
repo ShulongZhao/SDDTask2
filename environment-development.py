1057cd87767530262f9f6ec80936cddf5d92f7d4
xboxonemaddali9 <72016797+xboxonemaddali9@users.noreply.github.com>
2021-07-04 11:40:58 +1000

    Attempted to add layers

--- environment-development.py
+++ environment-development.py
@@ -3,29 +3,35 @@ import pygame
 
 pygame.init()
 
-class Sprites:
+class Sprites (pygame.sprite.Sprite):
+
     def __init__(self, fileLocation, size, pixelLocation):
+        pygame.sprite.Sprite().__init__(self)
+
         self.size = [int(size[0]), int(size[1])]
-        self.sprite = pygame.image.load(fileLocation)
-        # scaling
-        self.sprite = pygame.transform.scale(self.sprite, (int(size[0]), int(size[1])))
+        self.name = fileLocation[7:]
+        self.image = pygame.transform.scale(pygame.image.load(fileLocation), (int(size[0]), int(size[1])))
+        self.rect = self.image.get_rect()
         self.pixelLocation = pixelLocation
-
-
+        self.spriteGroup = None
 
 
 sprites = {
     "ground" : Sprites("Images/maxcheng2.bmp", [100.0, 100.0], [0, 0])
 }
 
- 
 scaleFactor = 1.5
+
+windowSize = (1280, 720)
 
 running = True
 while running:
 
-    window = pygame.display.set_mode((1280, 720))
+    window = pygame.display.set_mode(windowSize)
     window.fill((50, 50, 50))
+
+    for sprite in sprites:
+        sprites[sprite]
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -62,10 +68,12 @@ while running:
                         _sprite.size[1] *= scaleFactor
                     elif event.key == pygame.K_s:
                         _sprite.size[1] /= scaleFactor
-            
-                _sprite.sprite = pygame.transform.scale(_sprite.sprite, [int(_sprite.size[0]), int(_sprite.size[1])])
+                    _sprite.sprite = pygame.transform.scale(_sprite.sprite, [int(_sprite.size[0]), int(_sprite.size[1])])
 
-    window.blit(_sprite.sprite, _sprite.pixelLocation)
+    for sprite in sprites:
+        spriteGroup = pygame.sprite.Group()
+        spriteGroup.add(sprites[sprite])
+        spriteGroup.draw(window)
             
     pygame.display.update()
     
