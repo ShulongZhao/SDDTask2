cc751fb8f9f608e1f4ced65dcb127099d520b656
ShulongZhao <flatisjustice.shulong@gmail.com>
2021-07-07 19:21:54 +1000

    Created enemy class

--- Characters.py
+++ Characters.py
@@ -9,3 +9,12 @@ class Player:
         self.surface = None
         self.bulletImage = pygame.image.load(bulletImageFileLocation)
         self.animations = animations(_animDirectoryList)
+
+class Enemy:
+    def __init__(self, coordinates, size, speed, bombImageFileLocation, _animDirectoryList):
+        self.coordinates = coordinates
+        self.size = size
+        self.speed = speed
+        self.surface = None
+        self.bombImage = pygame.image.load(bombImageFileLocation)
+        self.animations = animations(_animDirectoryList)
